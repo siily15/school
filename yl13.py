6abc96364b21998c9fb57afcf9eef395e316e35d
siily15 <55434210+siily15@users.noreply.github.com>
2019-10-25 13:39:12 +0300

    Add files via upload

---
+++ yl13.py
@@ -0,0 +1,7 @@
+animal = input("Sisestage lemmikloom: " )
+print(animal[0])
+animallist= ["koer", "kass", "karu"]
+print(animallist)
+animallist.append(animal)
+print(animallist)
+print(animallist[1][-1])
---
+++ yl18.py
@@ -0,0 +1,2 @@
+for x in range (-5,0):
+    print(abs(x))
