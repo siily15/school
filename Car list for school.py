f8cb58323612b74dd2d0bd35e117a765e2238af3
siily15 <55434210+siily15@users.noreply.github.com>
2019-11-19 17:01:52 +0200

    Add files via upload

---
+++ Car list for school.py
@@ -0,0 +1,17 @@
+car = {
+    "toyota supra": "2019",
+        "color1": "white, red, black",
+    "nissan nismo 370z": "2009",
+        "color2": "white, red ",
+    "audi A8": "2006",
+        "color3": "grey, black, white, red",
+}
+
+
+
+
+
+
+
+for x in car:
+    print(car[x])
