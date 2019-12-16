ae7175e5c2ca3061856a82f7ad1a45f8e9aa0399
siily15 <55434210+siily15@users.noreply.github.com>
2019-12-16 14:07:56 +0200

    Update yl12.py

--- yl12.py
+++ yl12.py
@@ -1,32 +1,13 @@
-thislist = ["ploom","õun","pirn"]
-print(thislist)
-#listi esiene väärtus
-print(thislist[1])
-#lisa lõppus uus puuvili
-thislist.append("kirss")
-print(thislist)
-#väljasta listi viimane väärtus
-print(thislist[3])
-#muuda ühe elemendi väärtus ja väljesta kogu list
-thislist = ("ploom","õun","pirn","kirss")
-thislist = "banan"
-print(thislist)
-#kas õun ekstiteerib
-thislist = ("ploom","õun","pirn","kirss")
-if "õun" in thislist:
-    print("õun on jah, puuvili")
-#listi pikkus
-thislist = ("ploom","õun","pirn","kirss")
-print(len(thislist))
-#eemalda element
-thislist = ["ploom","õun","pirn","kirss"]
-thislist.remove("õun")
-print(thislist)
-#listi järjekord
-thislist = ["ploom","õun","pirn","kirss"]
-thislist.reverse()
-print(thislist)
-#listi sorteerimine
-thislist = ["ploom","õun","pirn","kirss"]
-thislist.sort()
-print(thislist)
+fruit = ['apple', 'orange', 'cherry']
+print(fruit[1])
+fruit.append('pineapple')
+print(fruit[-1])
+fruit[2] = 'banan'
+if 'apple' in fruit:
+    print('we found a apple')
+print(len(fruit))
+fruit.remove('orange')
+fruit.reverse()
+print(fruit)
+fruit.sort()
+print(fruit)
